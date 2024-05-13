6337d5fce50fa0c8dc6a5922062836a9d88e1659
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2024-05-13 12:47:33 +0530

    Increase heading size

--- addfascript.py
+++ addfascript.py
@@ -15,6 +15,18 @@ link_tag.attrs['href'] = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.
 # Add the link tag to the head
 soup.head.append(link_tag)
 
+# Find and remove all <span> tags with class 'titlemark'
+for tag in soup.find_all('span', {'class': 'titlemark'}):
+    tag.decompose()
+
+
+    # Find the <span> tag with class 'cmr-17x-x-143'
+h1tag = soup.find('span', {'class': 'cmr-17x-x-143'})
+
+# Modify the style attribute to set the font size and text alignment
+h1tag['style'] = 'font-size: 50px;  display: block;'
+
+
 # Write the modified HTML back to the file
 with open('website/index.html', 'w', encoding='utf-8') as file:
     file.write(str(soup))
