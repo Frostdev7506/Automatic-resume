cfde3b21401b4a81a35192a0c54be06a9e8f8cc2
Neeraj Butola <34231063+Frostdev7506@users.noreply.github.com>
2024-05-13 23:27:38 +0530

    new addfascript.py

--- addfascript.py
+++ addfascript.py
@@ -23,6 +23,86 @@ for tag in soup.find_all('span', {'class': 'titlemark'}):
     # Find the <span> tag with class 'cmr-17x-x-143'
 h1tag = soup.find('span', {'class': 'cmr-17x-x-143'})
 
+
+# Find the span tag with class 'cmr-10'
+span_tag = soup.find('span', class_='cmr-10')
+
+# Check if the span tag exists
+if span_tag is not None:
+    # Get the parent p tag of the span tag
+    p_tag = span_tag.find_parent('p')
+
+    # Check if the p tag exists before changing its style
+    if p_tag is not None:
+        p_tag['style'] = 'width: 100%; box-sizing: border-box; text-align: center; font-size: 20px;'
+    else:
+        print("No parent p tag found for the span tag with class 'cmr-10'")
+else:
+    print("No span tag with class 'cmr-10' found")
+
+# Find the body tag
+body_tag = soup.find('body')
+
+# Check if the body tag exists before changing its style
+if body_tag is not None:
+    body_tag['style'] = 'font-size: 18px;'
+else:
+    print("No body tag found")
+
+
+# Find the table containing the rows
+table = soup.find('table',id="TBL-27")
+
+# Add style to the table tag
+# table['style'] = 'border: 1px solid black; padding: 10px;'
+
+# Find all tr tags within the table
+rows = table.find_all('tr')
+
+# Add style to each tr tag
+for row in rows:
+    row['style'] = 'border: 1px solid black; padding: 10px;'
+
+
+
+
+# Find the table with id 'TBL-3'
+table = soup.find('table', id='TBL-3')
+
+# Check if the table exists before changing its style
+if table is not None:
+    # Add style to the table tag to take full width
+    table['style'] = 'border: 1px solid black; padding: 10px; width: 100%;'
+
+    # Find all p tags within the table
+    p_tags = table.find_all('p')
+
+    # Add style to each p tag to center the text
+    for p_tag in p_tags:
+        p_tag['style'] = 'width: 100%; box-sizing: border-box; text-align: center;'
+else:
+    print("No table with id 'TBL-3' found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Modify the style attribute to set the font size and text alignment
 h1tag['style'] = 'font-size: 50px;  display: block;'
 
